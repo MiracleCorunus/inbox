ccaa24b958df851b396a87e3315bdc972db8a966
MiracleCorunus <miraclegwon@gmail.com>
2021-07-12 15:39:46 +0900

    finish course 48

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -16,11 +16,11 @@ beforeEach(async () => {
 
     inbox = await new web3.eth.Contract(JSON.parse(interface))
         .deploy({ data: bytecode, arguments: ["Hi, there!"] })
-        .send({ from: accounts[0], gas: '1000000' })
+        .send({ from: accounts[0], gas: '1000000' });
 });
 
 describe('Inbox', () => {
     it('deploys a contract', () => {
-        console.log(inbox);
-    })
-})
+        assert.ok(inbox.options.address);
+    });
+});
