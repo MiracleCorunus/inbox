8b645a0ca9f2d469da91cb859a765f63e91c29d0
MiracleCorunus <miraclegwon@gmail.com>
2021-10-13 04:38:06 +0900

    deploy contract to rinkeby testnet

---
+++ deploy.js
@@ -0,0 +1,23 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
+
+const provider = new HDWalletProvider(
+    'join evidence illness illness vendor next okay friend reopen enact throw security',
+    'https://rinkeby.infura.io/v3/118b4a8511bd4a60933e1dd5c6157a39'
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+    const accounts = await web3.eth.getAccounts();
+    console.log('Attempting to deploy from account', accounts[0]);
+
+    const result = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({ data: bytecode, arguments: ['Hi there!'] })
+        .send({ gas: '1000000', from: accounts[0] });
+
+    console.log('Contract deployed to', result.options.address);
+};
+
+deploy();
